965e6effa77554c7a20495d102c51df5382d7c23
Magalum <jeonmagalum@gmail.com>
2025-10-03 08:12:35 -0500

    Resetear contraseña

--- app.py
+++ app.py
@@ -3,8 +3,44 @@ from flask import Flask, flash, redirect, render_template, request, url_for, ses
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
 from werkzeug.security import generate_password_hash, check_password_hash
-from werkzeug.utils import secure_filename
-import os
+
+import secrets
+from datetime import datetime, timedelta
+import smtplib
+from email.mime.text import MIMEText
+
+import secrets
+from datetime import datetime, timedelta
+import smtplib
+from email.mime.text import MIMEText
+
+def  generar_token (email):
+    token = secrets.token_urlsafe(32)
+    expiry=  datetime.now () + timedelta(hours =1)
+    cur = mysql.connection.cursor()
+    cur.execute("UPDATE usuarios SET reset_token= %s, token_expiry= %s WHERE username = %s", (token, expiry, email))
+    mysql.connection.commit()
+    cur.close()
+    return token
+def enviar_correo_reset(email,token):
+    enlace = url_for('reset', token = token, _external=True)
+    cuerpo = f"""Hola, solicitaste recuperar tu contraseña. Haz click en el siguiente enlace:
+    {enlace}
+    Este enlace expirará en 1 hora.
+    Si no lo solicitó, ignore este mensaje. """
+
+    remitente = 'jeonmagalum@gmail.com'
+    clave = 'twwg oxei qcrg inyd'
+    mensaje = MIMEText(cuerpo)
+    mensaje['Subject'] = 'Recuperar contraseña'
+    mensaje['From'] = 'jeonmagalum@gmail.com'
+    mensaje['To'] = email
+
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(remitente,clave)
+    server.sendmail(remitente,email,mensaje.as_string())
+    server.quit()
 
 app = Flask(__name__)
 app.config["MYSQL_HOST"]= "localhost" #servidor xampp
@@ -13,7 +49,7 @@ app.config["MYSQL_PASSWORD"]= ""
 app.config["MYSQL_DB"]= "paw_pet"  # nombre de la base
 
 mysql = MySQL(app)
-app.secret_key = 'tu_clave_secreta_super_segura'
+app.secret_key = 'Maya'
 @app.route("/")
 def index():
     #usuamos render_template para mostar el archivo 'index,html'
@@ -68,12 +104,58 @@ def registrarse():
             cur.close()
     return render_template('registrarse.html')
 
-    
-@app.route('/recuperar_contraseña')
+@app.route('/recuperar_contraseña',methods=['GET','POST'])
 def recuperar_contraseña():
+    if request.method =='POST':
+        email = request.form['email']
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT idUsuario FROM usuarios WHERE username = %s", (email,))
+        existe = cur.fetchone()
+        cur.close()
+
+        if not existe:
+          flash("Este correo no está registrado.")
+          return redirect(url_for('recuperar_contraseña'))   
+     
+        token = generar_token(email)
+        enviar_correo_reset(email,token)
+
+        flash("Se le envío un correo con el enlace para restablecer su contraseña")
+        return redirect(url_for('login'))
     return render_template('recuperar_contraseña.html')
+    
+    #Ruta recuperar contraseña
+@app.route('/reset/<token>', methods = ['GET','POST'])
+def reset (token):
+    cur =mysql.connection.cursor()
+    cur.execute("SELECT idUsuario, token_expiry FROM usuarios WHERE reset_token = %s", (token,))
+    usuario = cur.fetchone()
+    cur.close()
 
+    if not usuario or datetime.now() >usuario [1]:
+        flash ("token inválido o expirado.")
+        return redirect(url_for('recuperar_contraseña'))
+    
+    if request.method =='POST':
+        nuevo_password = request.form['password']
+        hash_nueva = generate_password_hash(nuevo_password)
+        
+        cur = mysql.connection.cursor()
+        cur.execute("UPDATE usuarios SET password=%s, reset_token=NULL, token_expiry=NULL WHERE idUsuario=%s", (hash_nueva, usuario[0]))
+        mysql.connection.commit()
+        cur.close()
 
+        flash ("Su contraseña ha sido actualizada.")
+        return redirect(url_for('login')) 
+
+    return render_template('reset.html')
+
+     
+
+@app.route('/inventario')
+def inventario():
+    #if 'rol' not in session or session['rol'] !='admin'
+    return render_template('inventario.html')
 
 @app.route('/sobre_nosotras')
 def sobre_nosotras ():
@@ -99,42 +181,6 @@ def eliminar(id):
     cursor.close()
     flash ('Usuario eliminado')
     return redirect(url_for('dashboard'))
-
-@app.route('/inventario')
-def inventario():
-    #if 'rol' not in session or session['rol'] != 'admin':
-    #    flash("acceso  restringido solo parfa los administradores")
-    #    return redirect(url_for('login'))
-    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-    cursor.execute("SELECT * FROM productos")
-    productos = cursor.fetchall()
-    cursor.close()
-    return render_template('inventario.html', productos=productos)
-
-@app.route('/agregar_producto', methods=['GET','POST'])
-def agregar_producto():
-    #if 'rol' not in session or session['rol'] != 'admin':
-    #   flash("acceso  restringido solo parfa los administradores")
-    #    return redirect(url_for('login'))
-    if request.method=='POST':
-        nombre=request.form['nombre']
-        descripcion=request.form['descripcion']
-        precio=request.form['precio']
-        imagen=request.files['imagen']
-        cantidad=request.form['cantidad']
-        
-        filename=secure_filename(imagen.filename)
-        imagen.save(os.path.join('static/uploads',filename))
-        cursor = mysql.connection.cursor()
-        cursor.execute("INSERT INTO productos (nombre_producto,descripcion,precio,cantidad,imagen)VALUES(%s,%s,%s,%s,%s)",(nombre,descripcion,precio,cantidad,filename))
-        mysql.connection.commit()
-        cursor.close()
-        
-        
-        flash("producto almacenado correctamente")
-        return redirect(url_for('inventario'))
-    return render_template('agregar_producto.html')
-
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
